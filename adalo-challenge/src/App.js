eaf27157da570160b283b771cb1e6f3f963c5349
Jensen Koch <jtkoch12@gmail.com>
2020-03-30 14:35:55 -0500

    Table on screen, Need to add the new column functionality

--- adalo-challenge/src/App.js
+++ adalo-challenge/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
+import * as ReactBootStrap from 'react-bootstrap';
 
-function App() {
-  return (
+const App = () => {
+  const [data, setData] = useState([
+    {name: "Bianchi", description: "Road Bike", price: "$3000"},
+    {name: "Surly", description: "Tour Bike", price: "$1500"},
+    {name: "Peugeot", description: "Vintage Road Bike", price: "$800"},
+    {name: "Yeti", description: "Mountain Bike", price: "$4500"}
+  ]);
+
+  console.log(data);
+
+  const renderData = (data, index) => {
+    return(
+      <tr key={index} >
+        <td>{data.name}</td>
+        <td>{data.description}</td>
+        <td>{data.price}</td>
+      </tr>
+    );
+  }
+
+  return(
     <div className="App">
-      <h1>App</h1>
+      <ReactBootStrap.Table striped bordered hover>
+        <thead className="table-head">
+          <tr className="table-row">
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody className="table-body">
+          {data.map(renderData)}
+        </tbody>
+      </ReactBootStrap.Table>
     </div>
-  );
+  )
 }
 
 export default App;
--- adalo-challenge/src/index.js
+++ adalo-challenge/src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
